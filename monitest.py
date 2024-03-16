2b31ae64a9df71baa92e81f29f179980e3a4a81b
tianli052124 <tianli052124@gmail.com>
2024-03-16 14:23:17 +0800

    environment change

--- monitest.py
+++ monitest.py
@@ -1,3 +1,4 @@
+import json
 import okx.Account as Account
 api_key = "e1b9fa18-438f-4186-8679-2e1a31cac369"
 secret_key = "ED6A1408691C36597446782AA57D8BC3"
@@ -5,5 +6,5 @@ passphrase = "Llz0102!!"
 
 flag = "1"  # live trading: 0, demo trading: 1
 accountAPI = Account.AccountAPI(api_key, secret_key, passphrase, False, flag)
-result = accountAPI.get_account_balance()
-print(result)
+result = accountAPI.get_positions()
+print(json.dumps(result,sort_keys=True,indent=4))
